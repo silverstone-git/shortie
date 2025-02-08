7ab1600753b0df60fb57ad769c9d88fcb53957a2
cyto <silverstone965@gmail.com>
2025-02-08 10:14:34 +0530

    SET trust proxy 1

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import authMiddleware from '@/middlewares/auth.middleware';
 import 'dotenv/config'
 
 const app = e();
-app.set("trust proxy", true)
+app.set("trust proxy", 1)
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
